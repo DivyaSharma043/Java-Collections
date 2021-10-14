930e8a315d5f8fe3a1be4406fc2905220a3db3e1
DIVYA SHARMA <divya@racloop.com>
2021-10-14 13:10:37 +0530

    Tree

---
+++ src/main/java/TreeProject.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class TreeProject {
+    public static void main(String[] args) {
+        
+        TreeSet<Integer>ts;
+        ts = new TreeSet<>();
+        ts.add(20);
+        ts.add(17);
+        ts.add(6);
+        ts.add(8);
+        ts.add(10);
+        ts.add(7);
+        ts.add(18);
+        ts.add(13);
+        ts.add(12);
+        ts.add(5);
+        
+        System.out.println(ts);
+        System.out.println(ts.subSet(6,17));
+        ts.remove(18);
+        System.out.println(ts);
+        ts.clear();
+        System.out.println(ts);
+        
+    }
+}
